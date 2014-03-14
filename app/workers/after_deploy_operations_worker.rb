3c5ec21b27116ced984f44d07cbc4f92af7c56fa
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2014-03-14 11:25:24 +0400

    limit after deploy operations fails

--- app/workers/after_deploy_operations_worker.rb
+++ app/workers/after_deploy_operations_worker.rb
@@ -1,6 +1,13 @@
 class AfterDeployOperationsWorker
   include Sidekiq::Worker
 
+  sidekiq_options retry: 3
+
+  sidekiq_retry_in do |count|
+    # 1, 8, 27
+    1.hour * (count + 1)**3
+  end
+
   def perform(deploy_id)
     deploy = Deploy.find(deploy_id)
 
