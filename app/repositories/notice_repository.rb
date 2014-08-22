450faf61e13482b15ea2d59d76848289fcc697e2
Mikhail Kuzmin <m.kuzmin@darkleaf.ru>
2014-08-22 12:35:25 +0400

    use id for notice ordering

--- app/repositories/notice_repository.rb
+++ app/repositories/notice_repository.rb
@@ -2,8 +2,8 @@ module NoticeRepository
   extend ActiveSupport::Concern
 
   included do
-    scope :ordered, -> { reorder('created_at asc') }
-    scope :reverse_ordered, -> { reorder('created_at desc') }
+    scope :ordered, -> { reorder(id: :asc) }
+    scope :reverse_ordered, -> { reorder(id: :desc) }
     scope :for_errs, ->(errs) { where(err_id: errs.pluck(:id)) }
     scope :created_between, ->(start_date, end_date) { where(created_at: start_date..end_date) }
   end
